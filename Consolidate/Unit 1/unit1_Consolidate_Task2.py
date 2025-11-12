fc657da482b1c6711a6e7ff5bbdf662b802e9de2
TalhaUsman06 <91587070+TalhaUsman06@users.noreply.github.com>
2025-11-12 11:53:31 +0000

    Unit 1 Consolidate Tasks

---
+++ Consolidate/Unit 1/unit1_Consolidate_Task2.py
@@ -0,0 +1,10 @@
+"""
+Unit 1 Consolidate-Task 2
+"""
+
+
+print("\nWelcome to the Software Development unit")
+
+print("\nHello, my name is TALHA")
+
+print("\nLearning Python is easy, and I am really enjoying it!")
